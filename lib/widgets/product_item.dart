20242ca6a695a6bf79b29a88948be72d8c36b319
Paige <paigekim29@gmail.com>
2022-03-02 13:59:08 +0900

    [FEAT] remove cart item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -58,6 +58,24 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added item into the cart!',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: Duration(
+                    seconds: 2,
+                  ),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).colorScheme.secondary,
           ),
